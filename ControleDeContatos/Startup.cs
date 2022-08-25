958a9aa6330a1cba9a0c7921e6d7bb8e65f5189a
wallacefelipe21 <wallacefelipe21@outlook.com>
2022-08-25 19:59:42 -0300

    Inicio da implementação do DB

---
+++ ControleDeContatos/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace ControleDeContatos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World");
+                });
+            });
+        }
+    }
+}
